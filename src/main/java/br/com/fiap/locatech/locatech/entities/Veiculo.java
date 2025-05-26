4ac1afae1a3abd3fd4145c4ce0d67bd7cf687277
LuisVanique <cunhagustavo142@gmail.com>
2025-05-25 23:51:26 -0300

    commit alteracoes

--- src/main/java/br/com/fiap/locatech/locatech/entities/Veiculo.java
+++ src/main/java/br/com/fiap/locatech/locatech/entities/Veiculo.java
@@ -4,7 +4,7 @@ import lombok.*;
 
 import java.math.BigDecimal;
 
-@Getter
+
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -30,9 +30,6 @@ public class Veiculo {
         return cor;
     }
 
-    public Long getId() {
-        return id;
-    }
 
     public String getMarca() {
         return marca;
